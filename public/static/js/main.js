3eba2f6add084d42e291de23019438a0a3838f23
Pascal Wittler <git@pascalwittler.de>
2023-07-08 01:06:16 +0200

    add js functionality to show/hide text blocks

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -18,4 +18,26 @@ document.addEventListener('DOMContentLoaded', () => {
       'minutes': minutesRounded
     };
   }
+
+  function showAndHideWords() {
+    const now = getCurrentTime();
+
+    document.querySelectorAll('li[data-hours], li[data-minutes]').forEach((element) => {
+      element.classList.remove('active');
+    });
+
+    const hoursToActivate = document.querySelectorAll(`li[data-hours~='${now.hours}']`);
+    const minutesToActivate = document.querySelectorAll(`li[data-minutes~='${now.minutes}']`);
+
+    hoursToActivate.forEach((element) => {
+      element.classList.add('active');
+    });
+
+    minutesToActivate.forEach((element) => {
+      element.classList.add('active');
+    });
+  }
+
+  showAndHideWords();
+  setInterval(showAndHideWords, 1000);
 });
